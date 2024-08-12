6fcd3753a00c48d57fb7365378c0a897ccfc4ac1
J <flashsugarfellows@gmail.com>
2024-08-11 20:36:46 -0500

    Initial Update

--- src/main/java/moe/hertz/infinity_fix/InfinityFix.java
+++ src/main/java/moe/hertz/infinity_fix/InfinityFix.java
@@ -1,12 +1,57 @@
 package moe.hertz.infinity_fix;
 
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.fabricmc.fabric.api.item.v1.EnchantingContext;
+import net.fabricmc.fabric.api.item.v1.EnchantmentEvents;
+import net.fabricmc.fabric.api.util.TriState;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.World;
+import java.util.EventListener;
+import net.minecraft.server.world.ServerWorld;
 
+public class InfinityFix implements net.fabricmc.api.ModInitializer {
 
-public class InfinityFix implements ModInitializer {
+    public static ServerWorld currentWorld;
     @Override
     public void onInitialize() {
-        // TODO
+
+        EnchantmentEvents.ALLOW_ENCHANTING.register(this::canEnchantTogether);
+        ServerWorldEvents.LOAD.register(this::setCurrentWorld);
     }
+
+
+
+    private void setCurrentWorld(MinecraftServer minecraftServer, ServerWorld serverWorld) {
+        currentWorld = serverWorld;
+    }
+
+
+
+
+    private TriState canEnchantTogether(RegistryEntry<Enchantment> enchantmentRegistryEntry, ItemStack stack, EnchantingContext enchantingContext) {
+
+        if(stack.getItem() == Items.CROSSBOW) {
+                enchantmentRegistryEntry.getKeyOrValue();
+
+                if (enchantmentRegistryEntry.matchesKey(Enchantments.INFINITY) || enchantmentRegistryEntry.matchesKey(Enchantments.MENDING) || enchantmentRegistryEntry.matchesKey(Enchantments.POWER)) {
+                    return TriState.TRUE;
+
+                }
+
+
+            }
+        System.out.println("returning default");
+        return TriState.DEFAULT;
+
+    }
+
+
 }
---
+++ src/testMod/java/moe/hertz/infinity_fix/test/TestInfinityFix.java
@@ -0,0 +1,83 @@
+package moe.hertz.infinity_fix.test;
+import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.fabricmc.fabric.api.item.v1.EnchantingContext;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.test.*;
+import net.minecraft.item.Items;
+import net.minecraft.test.TestContext;
+import net.minecraft.util.Identifier;
+import net.minecraft.registry.DynamicRegistryManager;
+
+
+public class TestInfinityFix {
+
+    private static final String TEMPLATE = "infinity_fix-testmod:1x2x1_chamber";
+    @GameTest(templateName = TEMPLATE,tickLimit =  200)
+    public void testInfinityWithMendingBow(TestContext context)
+    {
+        var world = context.getWorld();
+        DynamicRegistryManager dynamicRegistryManager = world.getRegistryManager();
+        RegistryEntry<Enchantment> infinity = dynamicRegistryManager.get(RegistryKeys.ENCHANTMENT).getEntry(Identifier.ofVanilla("infinity")).get();
+        RegistryEntry<Enchantment> mending = dynamicRegistryManager.get(RegistryKeys.ENCHANTMENT).getEntry(Identifier.ofVanilla("mending")).get();
+
+        var bow = Items.BOW.getDefaultStack();
+        bow.addEnchantment(infinity,1);
+        var IsMendingCompat = bow.canBeEnchantedWith(mending, EnchantingContext.ACCEPTABLE);
+        System.out.println("IsMendingCompat"+ IsMendingCompat);
+        var bow2 = Items.BOW.getDefaultStack();
+        bow2.addEnchantment(mending,1);
+        context.assertTrue(IsMendingCompat,"mending is able to be added after Infinity");
+
+        var IsInfinityCompat = bow2.canBeEnchantedWith(infinity,EnchantingContext.ACCEPTABLE);
+        System.out.println("IsMendingCompat"+ IsInfinityCompat);
+        context.assertTrue(IsInfinityCompat,"Infinity is able to be added after Mending");
+
+        context.complete();
+
+    }
+    @GameTest(templateName = TEMPLATE,tickLimit =  200)
+    public void testInfinityBowWithPower(TestContext context)
+    {
+        var world = context.getWorld();
+        DynamicRegistryManager dynamicRegistryManager = world.getRegistryManager();
+        RegistryEntry<Enchantment> infinity = dynamicRegistryManager.get(RegistryKeys.ENCHANTMENT).getEntry(Identifier.ofVanilla("infinity")).get();
+        RegistryEntry<Enchantment> power = dynamicRegistryManager.get(RegistryKeys.ENCHANTMENT).getEntry(Identifier.ofVanilla("power")).get();
+
+        var bow = Items.BOW.getDefaultStack();
+        bow.addEnchantment(infinity,1);
+        var IsPowerCompat = bow.canBeEnchantedWith(power, EnchantingContext.ACCEPTABLE);
+        context.assertTrue(IsPowerCompat,"Power is compatible with infinity");
+
+
+        context.complete();
+
+    }
+
+
+    @GameTest(templateName = TEMPLATE,tickLimit =  200)
+    public void testInfinityWithMendingCrossbow(TestContext context)
+    {
+        var world = context.getWorld();
+        DynamicRegistryManager dynamicRegistryManager = world.getRegistryManager();
+        RegistryEntry<Enchantment> infinity = dynamicRegistryManager.get(RegistryKeys.ENCHANTMENT).getEntry(Identifier.ofVanilla("infinity")).get();
+        RegistryEntry<Enchantment> mending = dynamicRegistryManager.get(RegistryKeys.ENCHANTMENT).getEntry(Identifier.ofVanilla("mending")).get();
+
+        var bow = Items.CROSSBOW.getDefaultStack();
+        bow.addEnchantment(infinity,1);
+        var IsMendingCompat = bow.canBeEnchantedWith(mending, EnchantingContext.ACCEPTABLE);
+        System.out.println("IsMendingCompat"+ IsMendingCompat);
+        var bow2 = Items.CROSSBOW.getDefaultStack();
+        bow2.addEnchantment(mending,1);
+        context.assertTrue(IsMendingCompat,"mending is able to be added after Infinity");
+
+        var IsInfinityCompat = bow2.canBeEnchantedWith(infinity,EnchantingContext.ACCEPTABLE);
+        System.out.println("IsMendingCompat"+ IsInfinityCompat);
+        context.assertTrue(IsInfinityCompat,"Infinity is able to be added after Mending");
+
+        context.complete();
+
+    }
+}
+
